f36296d97e071c8f13e5ace0cd6a46ea1bb62717
Sovann <cheasovann3344@gmail.com>
2026-06-27 17:49:07 +0700

    session2.2: add Form Requests and UserResource

---
+++ laravel-app/app/Http/Requests/User/SigninRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class SigninRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email|exists:users,email',
+            'password' => 'required|string|min:6|max:10'
+        ];
+    }
+}
