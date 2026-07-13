b4579824037fba561951129b6992c292fe6ca871
Sovann <cheasovann3344@gmail.com>
2026-07-13 14:01:36 +0700

    Session 3.3: add email verification backend

--- vuejs-app/src/main.js
+++ vuejs-app/src/main.js
@@ -3,13 +3,41 @@ import 'admin-lte/dist/js/adminlte.min.js';
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
+import { useUserStore } from '@/stores/user';
+import { apiVerify } from '@/functions/api/auth';
 
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+app.use(pinia);
+app.use(router);
+app.mount('#app');
 
-app.mount('#app')
+
+const userStore = useUserStore();
+router.beforeEach(async (to, from) => {
+  const { guarded } = to.meta;
+  if (guarded === undefined) {
+    return;
+  }
+
+  try {
+    const token = userStore.getSanctumToken();
+    const response = await apiVerify(token);
+    const { data } = response;
+    userStore.setState(data.user);
+  } catch (error) {
+    userStore.reset();
+  }
+
+  if (guarded && !userStore.isAuthenticated) {
+    return { name: 'auth.signin' };
+  }
+  if (!guarded && userStore.isAuthenticated) {
+    return { name: 'dashboard' };
+  }
+});
